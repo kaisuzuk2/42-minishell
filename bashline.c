e5c6277636f378d05bac5bbc8afadfeaa073c825
Kaisei Suzuki <kaisuzuk@c3r6s2.42tokyo.jp>
2025-10-06 13:51:27 +0900

    絶対パスの実行

--- bashline.c
+++ bashline.c
@@ -1,24 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   bashline.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kaisuzuk <kaisuzuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/06 13:12:50 by kaisuzuk          #+#    #+#             */
-/*   Updated: 2025/10/06 13:40:25 by kaisuzuk         ###   ########.fr       */
+/*   Created: 2025/10/06 13:21:40 by kaisuzuk          #+#    #+#             */
+/*   Updated: 2025/10/06 13:31:48 by kaisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#include "minishell.h"
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-
-
-#endif
+// TODO: readlineの設定を考える
+// https://tiswww.case.edu/php/chet/readline/readline.html
+void initialize_readline(void)
+{
+	rl_instream = stdin;
+	rl_outstream = stderr;
+}
--- minishell.c
+++ minishell.c
@@ -6,11 +6,48 @@
 /*   By: kaisuzuk <kaisuzuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 13:13:31 by kaisuzuk          #+#    #+#             */
-/*   Updated: 2025/10/06 13:17:43 by kaisuzuk         ###   ########.fr       */
+/*   Updated: 2025/10/06 13:49:34 by kaisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+// tmp code ↓
+
+#include <stdio.h>
+#include <sys/types.h>
+
+void fatal_error(const char *msg)  __attribute__((noreturn));
+
+void fatal_error(const char *msg)
+{
+	dprintf(STDERR_FILENO, "fatal Error: %s\n", msg);
+	exit(1);
+}
+
+int interpret(char *line)
+{
+	extern char **environ;
+	char *argv[] = {line, NULL};
+	pid_t pid;
+	int wstatus;
+
+	pid = fork();
+	if (pid < 0)
+		fatal_error("fork");
+	else if (pid == 0)
+	{
+		execve(line, argv, environ);
+		fatal_error("execve");
+	}
+	else
+	{
+		wait(&wstatus);
+		return (WEXITSTATUS(wstatus));
+	}
+}
+
+// tmp code ↑
 
 int main(void)
 {
@@ -25,6 +62,7 @@ int main(void)
 			break;
 		if (*line)
 			add_history(line);
+		interpret(line);
 		free(line);
 	}
 }
