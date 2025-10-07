3d6ffdd20ccdaba63c105958d83ccd885a3f130b
Kaisei Suzuki <kaisuzuk@c2r2s7.42tokyo.jp>
2025-10-07 11:46:44 +0900

    トークンの構造体を作成

---
+++ command.h
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   command.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaisuzuk <kaisuzuk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/07 10:53:00 by kaisuzuk          #+#    #+#             */
+/*   Updated: 2025/10/07 11:46:03 by kaisuzuk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COMMAND_H
+# define COMMAND_H
+
+/* **************************************************************** */
+/*								    */
+/*			Token Structs			    */
+/*								    */
+/* **************************************************************** */
+
+typedef enum e_token_kind
+{
+	TK_WORD, 
+	TK_EOF,
+} t_token_kind;
+
+typedef struct s_word_desc
+{
+	t_token_kind kind;
+	char *word;
+	int flag;
+} t_word_desc;
+
+typedef struct s_word_list
+{
+	struct s_word_list *next;
+	t_word_desc *word;
+} t_word_list; 
+
+#endif
