e8a70fa6e537836648af9ed973c7cd07ca5e589f
Kaisei Suzuki <kaisuzuk@c2r3s8.42tokyo.jp>
2025-10-29 16:01:20 +0900

    exportを修正g

--- builtin_export.c
+++ builtin_export.c
@@ -6,7 +6,7 @@
 /*   By: kaisuzuk <kaisuzuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 08:37:57 by kaisuzuk          #+#    #+#             */
-/*   Updated: 2025/10/29 15:52:55 by kaisuzuk         ###   ########.fr       */
+/*   Updated: 2025/10/29 16:00:36 by kaisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,42 +19,18 @@ static int show_var_attributes(t_varlist *env)
 	char **envstr;
 	int i;
 
-	envstr = get_env_arr(env); // ### TODO: エラー処理
+	envstr = get_env_arr(env);  // varlistのexportstr出力したらええのでは
 	if (!envstr)
-		return (99);
+		return (EX_FATAL_ERROR);
 	i = 0;
 	while (envstr[i])
 	{
 		ft_dprintf(STDOUT_FILENO, "declare -x %s\n", envstr[i]);
+		free(envstr[i]);
 		i++;
 	}
-	return (1);
+	return (EXECUTION_SUCCESS);
 }
-
-// int builtin_export(t_word_list *list, t_shell_env *shell_env)
-// {
-// 	t_varlist *env;
-
-// 	env = shell_env->env;
-// 	if (!list)
-// 		return (show_var_attributes(env));
-// 	while (env->next)
-// 		env = env->next;
-// 	if (!set_variable_value(env->var, list->word->word))
-// 		return (99); // ### TODO: エラー処理
-// 	if (!set_variable_name(env->var, list->word->word))
-// 		return (99); // ### TODO: エラー処理
-// 	if (!set_variable_exportstr(env->var, list->word->word))
-// 		return (99); // ### TODO: エラー処理
-// 	set_variable_attributes(env->var);
-// 	env->next = create_varlist();
-// 	if (!env->next)
-// 		return (99); // ### TODO: エラー処理
-// 	env->next->var = create_shell_var();
-// 	if (!env->next->var)
-// 		return (99); // ### TODO: エラー処理
-// 	return (1);
-// }
 
 static t_bool is_valid_env_name(char *exportstr)
 {
