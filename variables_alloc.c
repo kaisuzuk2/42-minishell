64a05676a5f0caf194580a437f086e6a0eae9e85
Kaisei Suzuki <kaisuzuk@c2r2s7.42tokyo.jp>
2025-11-15 15:25:21 +0900

    norm修正

--- variables_alloc.c
+++ variables_alloc.c
@@ -6,7 +6,7 @@
 /*   By: kaisuzuk <kaisuzuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 17:23:55 by kaisuzuk          #+#    #+#             */
-/*   Updated: 2025/11/11 16:41:29 by kaisuzuk         ###   ########.fr       */
+/*   Updated: 2025/11/15 15:25:07 by kaisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,14 @@ t_varlist	*create_varlist(void)
 
 	varlist = (t_varlist *)xcalloc(sizeof(t_varlist), 1);
 	if (!varlist)
-		return (NULL); 
+		return (NULL);
 	return (varlist);
 }
 
-char *create_exportstr(char *key, char *value)
+char	*create_exportstr(char *key, char *value)
 {
-	char *tmp;
-	char *res;
+	char	*tmp;
+	char	*res;
 
 	tmp = ft_strjoin(key, "=");
 	if (!tmp)
