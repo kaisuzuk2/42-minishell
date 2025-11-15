e69b7037cfc657af04399a13600b14590bb2aec5
Kaisei Suzuki <kaisuzuk@c2r2s7.42tokyo.jp>
2025-11-15 13:30:13 +0900

    norm修正

--- builtin_env.c
+++ builtin_env.c
@@ -6,33 +6,13 @@
 /*   By: kaisuzuk <kaisuzuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 11:26:48 by kaisuzuk          #+#    #+#             */
-/*   Updated: 2025/11/12 10:11:54 by kaisuzuk         ###   ########.fr       */
+/*   Updated: 2025/11/15 13:29:49 by kaisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// static int builtin_printenv(t_varlist *env)
-// {
-// 	char **envstr;
-// 	int i;
-
-// 	i = 0;
-// 	envstr = get_env_arr(env);
-// 	if (!envstr)
-// 		return (EX_FATAL_ERROR);
-// 	while (envstr[i])
-// 	{
-// 		ft_dprintf(STDOUT_FILENO, "%s\n", envstr[i]);
-// 		free(envstr[i]);
-// 		i++;
-// 	}
-// 	free(envstr);
-// 	return (EXECUTION_SUCCESS);
-// }
-
-
-static int builtin_printenv(t_varlist *env)
+static int	builtin_printenv(t_varlist *env)
 {
 	while (env)
 	{
@@ -47,9 +27,10 @@ static int builtin_printenv(t_varlist *env)
 	return (EXECUTION_SUCCESS);
 }
 
-int builtin_env(t_word_list *list, t_shell_env *shell_env)
+int	builtin_env(t_word_list *list, t_shell_env *shell_env)
 {
 	if (list)
-		return (builtin_error("env", list->word->word, NOSUP_ARG_ERR), EX_NOTFOUND);
+		return (builtin_error("env", list->word->word, NOSUP_ARG_ERR),
+			EX_NOTFOUND);
 	return (builtin_printenv(shell_env->env));
 }
